507aad3b46cb804057630803196e720ba7f9249f
sebastian <sebastian.gebala@gmail.com>
2016-07-20 21:03:08 +0200

    left-slide

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -6,9 +6,43 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top-100
-        }, 1000);
+        }, 500);
         return false;
       }
     }
   });
 });
+
+// Nav-Item 
+
+$(document).ready(function() {
+    $('.nav-item').click(function() {
+        $('.sidebar').toggleClass('closed');
+    });
+});
+
+// Toggle the blurred class
+
+$(document).ready(function(){
+
+function sidebar(){
+  var trigger = $('#trigger, #close'),
+  menu = $('.sidebar');
+
+  trigger.on('click',function(){
+   $(this).toggleClass('active');
+   menu.toggleClass('closed');
+  });
+}
+
+ function deploy(){
+  sidebar();
+ }
+
+ deploy();
+
+});
+
+
+
+
