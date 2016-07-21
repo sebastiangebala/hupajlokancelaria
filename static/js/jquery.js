7cdcbdd38b62b4f3595ca3bb23c1c08342d23f0f
sebastian <sebastian.gebala@gmail.com>
2016-07-21 21:41:10 +0200

    to top scroll and font awesome

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -6,7 +6,7 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top-100
-        }, 500);
+        }, 400);
         return false;
       }
     }
@@ -43,6 +43,25 @@ function sidebar(){
 
 });
 
+// Scroll to Top
 
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
 
 
