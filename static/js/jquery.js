df98b59b4a2ca883270375454f34a49278ae6323
sebastian <sebastian.gebala@gmail.com>
2016-07-30 19:05:55 +0200

    HupajloKancelaria 1.0

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -65,23 +65,71 @@ $(document).ready(function(){
 });
 
 // Shrinking nav
-var pagetop, menu, yPos;
+var pagetop, menu, right, kirp, pagetitle, yPos;
 function yScroll(){
 	pagetop = document.getElementById('pagetop');
 	menu = document.getElementById('menu');
+	right = document.getElementById('right');
+	kirp = document.getElementById('kirp');
+	pagetitle = document.getElementById('pagetitle');
 	yPos = window.pageYOffset;
 	if(yPos > 1){
 		pagetop.style.height = "50px";
 		pagetop.style.paddingTop = "0px";
 		pagetop.style.marginTop = "0px";
 		menu.style.height = "0px";
+		right.style.marginTop = "10px";
+		kirp.style.height = "49px";
+		kirp.style.width = "50px";
+		pagetitle.style.fontSize = "10px";
+		pagetitle.style.marginLeft = "-50px";
+		pagetitle.style.marginTop = "-5px";
 	} else {
 		pagetop.style.height = "100px";
-		pagetop.style.paddingTop = "50px";
+		pagetop.style.paddingTop = "0px";
 		pagetop.style.marginTop = "0px";
 		menu.style.height = "50px";
+		right.style.marginTop = "60px";
+		kirp.style.height = "99px";
+		kirp.style.width = "100px";
+		pagetitle.style.fontSize = "23px";
+		pagetitle.style.marginLeft = "0px";
+		pagetitle.style.marginTop = "15px";
 	}
 }
 window.addEventListener("scroll", yScroll);
 
+// Border-bottom for nav
+$(document).ready(function() {
+	$('.nav-selection').mouseenter(function() {
+		$(this).addClass("show-border");
+	});
+});
+$(document).ready(function() {
+	$('.nav-selection').mouseleave(function() {
+		$(this).removeClass("show-border");
+	});
+});
 
+//Partnerzy greyscalle
+$(document).ready(function() {
+	$('.partnerzy').mouseenter(function() {
+		$(this).removeClass("orlen");
+	});
+});
+$(document).ready(function() {
+	$('.partnerzy').mouseleave(function() {
+		$(this).addClass("orlen");
+	});
+});
+//Blurr 
+$(document).ready(function(){
+	$(window).scroll(function(){
+		yPos = window.pageYOffset;
+		if(yPos > 1){
+			$('.wizytowka').addClass("blurwizyt");
+		} else {
+			$('.wizytowka').removeClass("blurwizyt");
+		}
+	});
+});
